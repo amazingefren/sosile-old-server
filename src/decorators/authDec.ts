33154147a2ca038a376cbac5184866c03a70f2d2
amazingefren <efrentheroker@hotmail.com>
2021-08-06 20:42:39 -0600

    note error handling

--- src/decorators/authDec.ts
+++ src/decorators/authDec.ts
@@ -2,6 +2,7 @@ import { NextFunction } from "express";
 import { createMethodDecorator, UnauthorizedError } from "type-graphql";
 export default () => {
   return createMethodDecorator(async ({ context }: any, next: NextFunction) => {
+    // Gonna stick to using type-graphql built-in error handling
     return context.isAuth ? next() : new UnauthorizedError();
   });
 };
